6bee830196f548973a874dd09df0be2784653178
Łukasz Zieniewicz <lukzieniewicz@gmail.com>
2023-08-22 21:32:50 +0200

    Works :D

--- rest_api.py
+++ rest_api.py
@@ -1,6 +1,13 @@
+import io
+
+import torchaudio
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import StreamingResponse
+
+from tortoise.utils import audio
+from tortoise import api
 
 app = FastAPI()
 
@@ -13,26 +20,42 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+
+def generate_text(setnence: str):
+    clips_paths = [
+        '/src/tortoise/voices/train_empire/1.mp3',
+        '/src/tortoise/voices/train_empire/2.mp3',
+        '/src/tortoise/voices/train_empire/3.mp3',
+    ]
+    reference_clips = [audio.load_audio(p, 22050) for p in clips_paths]
+    tts = api.TextToSpeech(kv_cache=True, half=True)
+    audio_output = tts.tts_with_preset(setnence, voice_samples=reference_clips, preset='ultra_fast')
+    buffer = io.BytesIO()
+    torchaudio.save(buffer, audio_output.squeeze(0).cpu(), format="wav", sample_rate=24000)
+    buffer.seek(0)
+
+    # Return the audio data as a streaming response
+    return buffer
+
+
 class TTSRequest(BaseModel):
     sentence: str
     engine: str = 'tortoise'
 
 
-@app.get("/tts/")
-@app.get("/tts")
-#async def text_to_speech(data: TTSRequest):
-async def text_to_speech():
-    #sentence = data.sentence
-    #tts_engine = data.engine
+@app.post("/tts/")
+@app.post("/tts")
+async def text_to_speech(data: TTSRequest):
+    sentence = data.sentence
+    tts_engine = data.engine
 
-    #if not sentence:
-        #raise HTTPException(status_code=400, detail="Please provide a sentence.")
-    
-    # Instead of processing through TTS, just return the same text
-    #return {"text": sentence}
-    return {"text": "Testowy tekst"}
+    if not sentence:
+        raise HTTPException(status_code=400, detail="Please provide a sentence.")
+
+    buffer = generate_text(sentence)
+
+    return StreamingResponse(buffer, media_type="audio/wav")
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=5000)
-
